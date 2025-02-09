6a229334dacb386839991b3a110e280d9452a19d
Khrystyna <khrystyna.makar@gmail.com>
2025-02-09 12:35:58 +0200

    added project

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  basePath: "/NewWave4.org-frontend-new",
+  output: "export",
+  reactStrictMode: true,
+};
+
+export default nextConfig;
