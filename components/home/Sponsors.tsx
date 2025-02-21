774cccc33014c1c97fc5a591f79831a3b3ee7452
Khrystyna <khrystyna.makar@gmail.com>
2025-02-21 09:24:15 +0200

    fixed sponsors logos

--- components/home/Sponsors.tsx
+++ components/home/Sponsors.tsx
@@ -7,19 +7,19 @@ const Sponsors: React.FC = () => {
       <h4 className="preheader">Нам довіряють</h4>
       <div className="sponsors-logos">
         <Image
-          src={`${prefix}/sponsors/Logo_Credit_Union.png`}
+          src={`${prefix}/sponsors/Logo_Credit_Union.svg`}
           alt="Logo Credit-Union"
           width={250}
           height={42}
         />
         <Image
-          src={`${prefix}/sponsors/self-reliance-new-york.png`}
+          src={`${prefix}/sponsors/self-reliance-new-york.svg`}
           alt="Logo Self-Reliance-New-York"
           width={240}
           height={62}
         />
         <Image
-          src={`${prefix}/sponsors/Logo_Credit_Union.png`}
+          src={`${prefix}/sponsors/Logo_Credit_Union.svg`}
           alt="Logo Credit-Union"
           width={250}
           height={42}
