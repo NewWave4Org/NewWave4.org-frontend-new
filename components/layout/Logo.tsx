ff24f12b6259140e53b9f7bad676765dca8d8916
Khrystyna <khrystyna.makar@gmail.com>
2025-02-26 17:25:15 +0200

    fixed logo style gap

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const Logo = () => {
   return (
     <>
-      <Link href="/" className="flex w-fit gap-x-1">
+      <Link href="/" className="flex w-fit gap-x-2">
         <Image src={`${prefix}/logo.svg`} alt="logo" width={64} height={64} />
         <p className="w-[76px] text-small1 text-font-primary">
           Нова Українська Хвиля
