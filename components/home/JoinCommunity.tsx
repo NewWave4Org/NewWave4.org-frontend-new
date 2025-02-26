140c6e5130b4b4a4798c2032083622dd2e1dea19
Serhii Shevchyk <serhii.shevchyk@gmail.com>
2025-02-26 20:11:38 +0200

    #28 Updated Baskerville font

--- components/home/JoinCommunity.tsx
+++ components/home/JoinCommunity.tsx
@@ -7,12 +7,12 @@ const JoinCommunity = () => {
   return (
     <section className="our-mission py-14 my-20 bg-background-secondary">
       <h4 className="preheader mb-10 max-w-[1248px] text-center md:text-left mx-auto">{data.title}</h4>
-      <div className="flex flex-col items-center gap-6 md:flex-row md:justify-between max-w-[1248px] mx-auto">
+      <div className="flex flex-col items-top gap-6 md:flex-row md:justify-between max-w-[1248px] mx-auto">
         {data.items.map((item, i) => {
           return (
             <div key={item.title} className="flex w-full flex-col items-center text-center md:w-1/3 px-2 md:px-0">
-              <h4 className="mb-4 flex items-baseline text-font-accent text-h4 font-baskervville gap-4">
-                <span className="text-5xl font-bold text-primary-700">0{i + 1}</span>
+              <h4 className="mb-4 flex items-baseline text-font-accent text-h4 font-baskerville gap-4">
+                <span className="text-5xl font-bold text-primary-700 font-baskerville-bold">0{i + 1}</span>
                 <span className="text-2xl">{item.title}</span>
               </h4>
               <p className="text-font-primary text-base text-justify">{item.description}</p>
