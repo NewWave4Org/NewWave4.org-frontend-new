e522d2f8ec3c27a5ff78ae1492616d611d071690
Serhii Shevchyk <serhii.shevchyk@gmail.com>
2025-03-13 03:00:05 +0200

    #230 Fixed bug with text color

--- components/home/OurMission.tsx
+++ components/home/OurMission.tsx
@@ -11,10 +11,10 @@ const OurMission = () => {
       <div className="flex flex-col items-center gap-6 md:flex-row md:justify-between max-w-[1248px] mx-auto">
         {data.items.map((item) => {
           return (
-            <div key={item.title} className="flex w-full flex-col items-center text-center md:w-1/3 md:px-12">
+            <div key={item.title} className="flex w-full flex-col items-center text-center md:w-1/3 md:px-12 text-font-primary">
               <Image src={item.icon} alt={item.title} width={48} height={48} className="mb-4" />
-              <h4 className="mb-3 text-font-accent text-h4 text-2xl font-ebGaramond">{item.title}</h4>
-              <p className="text-font-primary text-base">{item.description}</p>
+              <h4 className="mb-3 text-h4 text-2xl font-ebGaramond">{item.title}</h4>
+              <p className="text-base">{item.description}</p>
             </div>
         );})}
       </div>
