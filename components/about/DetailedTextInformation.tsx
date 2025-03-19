2ea26f8c44af11e29a38c330dbb0c908db70f374
Serhii Shevchyk <serhii.shevchyk@gmail.com>
2025-03-19 13:21:33 +0200

    225 Fixed bug with the background

--- components/about/DetailedTextInformation.tsx
+++ components/about/DetailedTextInformation.tsx
@@ -1,4 +1,5 @@
 import React, { JSX } from 'react';
+import { prefix } from '@/utils/prefix';
 
 type DetailedTextInformationProps = {
   text: {
@@ -8,10 +9,15 @@ type DetailedTextInformationProps = {
 };
 
 const DetailedTextInformation = ({ text }: DetailedTextInformationProps) => {
+  const beforeBg = `${prefix}/background/decor-element-yellow.svg`;
+  const afterBg = `${prefix}/background/decor-element-blue.svg`;
+
   return (
     <section className='detailed-text-information'>
       <div className="container mx-auto">
-        <div className="container__inner max-w-[1036px] mx-auto text-center bg-gray-50 p-8 my-[80px] shadow-custom font-medium text-lg text-font-accent font-helv relative after:absolute after:bg-[url('/background/decor-element-blue.svg')] after:bg-cover after:bg-no-repeat after:content-[''] after:w-[160px] after:h-[160px] after:top-[-50px] after:right-6 before:absolute before:bg-[url('/background/decor-element-yellow.svg')] before:bg-cover before:bg-no-repeat before:content-[''] before:w-[160px] before:h-[160px] before:bottom-[-50px] before:left-6">
+        <div className="container__inner max-w-[1036px] mx-auto text-center bg-gray-50 p-8 my-[80px] shadow-custom font-medium text-lg text-font-accent font-helv relative">
+          <div className="before-bg absolute bg-cover bg-no-repeat content-[''] w-[160px] h-[160px] bottom-[-50px] left-6" style={{backgroundImage: `url(${beforeBg})`}}></div>
+          <div className="after-bg absolute bg-cover bg-no-repeat content-[''] w-[160px] h-[160px] top-[-50px] right-6" style={{backgroundImage: `url(${afterBg})`}}></div>
           <div className="text flex flex-col gap-y-4 max-w-[612px] mx-auto">
             <div>{text.top}</div>
             <div>{text.bottom}</div>
