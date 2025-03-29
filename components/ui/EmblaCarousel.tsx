ed5ab131b6b4a2fc5e6fd3f3fe9b1d1229d26bd2
Serhii Shevchyk <serhii.shevchyk@gmail.com>
2025-03-30 00:11:41 +0200

    #83 Added History block

---
+++ components/ui/EmblaCarousel.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { EmblaOptionsType } from 'embla-carousel';
+import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
+import Fade from 'embla-carousel-fade';
+
+const EmblaCarousel = ( { slides, options = {} } : { slides: React.ReactNode[], options?: EmblaOptionsType }) => {
+  
+  const plugins = options.loop ? [
+    Autoplay({ playOnInit: true }),
+    Fade()
+  ] : [Fade()];
+
+  const [emblaRef] = useEmblaCarousel(options, plugins);
+
+  return (
+    <section className="embla relative">
+      <div className="embla__viewport overflow-hidden max-w-[718px] h-[200px] md:h-[524px]" ref={emblaRef}>
+        <div className="embla__container flex">
+          {slides.map((slide, index) => (
+            <div className="embla__slide !w-[718px] max-w-full" key={index}>
+              {slide}
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EmblaCarousel;
