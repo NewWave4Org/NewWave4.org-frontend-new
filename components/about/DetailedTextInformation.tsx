1290e2c2e2f6ce6251de451a9b7446530f97a185
Serhii Shevchyk <serhii.shevchyk@gmail.com>
2025-04-07 20:40:18 +0300

    Fixed small bugs

--- components/about/DetailedTextInformation.tsx
+++ components/about/DetailedTextInformation.tsx
@@ -15,9 +15,9 @@ const DetailedTextInformation = ({ text }: DetailedTextInformationProps) => {
   return (
     <section className='detailed-text-information'>
       <div className="container mx-auto">
-        <div className="container__inner max-w-[1036px] mx-auto text-center bg-gray-50 p-8 my-[80px] shadow-custom font-medium text-lg text-font-accent font-helv relative">
-          <div className="before-bg absolute bg-cover bg-no-repeat w-[160px] h-[160px] bottom-[-50px] left-6 z-2" style={{backgroundImage: `url(${beforeBg})`}}></div>
-          <div className="after-bg absolute bg-cover bg-no-repeat w-[160px] h-[160px] top-[-50px] right-6 z-2" style={{backgroundImage: `url(${afterBg})`}}></div>
+        <div className="container__inner max-w-[1036px] mx-auto text-center bg-gray-50 px-8 py-[48px] my-[80px] shadow-custom font-medium text-lg text-font-accent font-helv relative">
+          <div className="before-bg absolute bg-cover bg-no-repeat w-[160px] h-[160px] bottom-[-42px] left-6 z-2" style={{backgroundImage: `url(${beforeBg})`}}></div>
+          <div className="after-bg absolute bg-cover bg-no-repeat w-[160px] h-[160px] top-[-42px] right-6 z-2" style={{backgroundImage: `url(${afterBg})`}}></div>
           <div className="text flex flex-col gap-y-4 max-w-[612px] mx-auto relative z-10">
             <div>{text.top}</div>
             <div>{text.bottom}</div>
