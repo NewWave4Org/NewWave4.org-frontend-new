6aeaf423d684e79589d0dd166c2c2ed395b75545
dhanur350 <dhanur297@gamil.com>
2025-07-13 18:59:09 +0530

    feat: stripe checkout gateway integration

--- app/(payment)/donation/page.tsx
+++ app/(payment)/donation/page.tsx
@@ -3,16 +3,21 @@ import Logo from '@/components/layout/Logo';
 import PaymentForm from '@/components/payment/PaymentForm';
 import { paypalClientId } from '@/components/Payment/PaypalComponent';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 const DonationPage = () => {
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEYS!);
   return (
     <PayPalScriptProvider options={paypalClientId}>
-      <section className="container mx-auto payments-wrapper">
-        <header>
-          <Logo />
-        </header>
-        <PaymentForm />
-      </section>
+      <Elements stripe={stripePromise}>
+        <section className="container mx-auto payments-wrapper">
+          <header>
+            <Logo />
+          </header>
+          <PaymentForm />
+        </section>
+      </Elements>
     </PayPalScriptProvider>
   );
 };
