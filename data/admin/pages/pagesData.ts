1a247c3274d15763102f47ba7549ddbb579a4b37
Dasha <dasha2312s3@gmail.com>
2025-07-31 15:55:50 +0300

    added title for page Pages on admin panel

---
+++ data/admin/pages/pagesData.ts
@@ -0,0 +1,22 @@
+export const pagesDataTitles = [
+  {
+    id: 1,
+    title: 'Home page',
+  },
+  {
+    id: 2,
+    title: 'About Us page',
+  },
+  {
+    id: 3,
+    title: 'Projects page',
+  },
+  {
+    id: 4,
+    title: 'Program donation',
+  },
+  {
+    id: 5,
+    title: 'Contacts',
+  },
+];
