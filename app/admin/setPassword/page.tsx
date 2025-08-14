f83584292cc70ed89d8ecb2614f152656cb661e7
Dasha <dasha2312s3@gmail.com>
2025-08-14 11:58:43 +0300

    fix: added Suspense

--- app/admin/setPassword/page.tsx
+++ app/admin/setPassword/page.tsx
@@ -2,12 +2,17 @@
 
 import SetPasswordForm from '@/components/admin/SetPasswordForm/SetPasswordForm';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 function SetPasswordPage() {
   const searchParams = useSearchParams();
   const token = searchParams.get('token') ?? '';
 
-  return <SetPasswordForm token={token} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SetPasswordForm token={token} />;
+    </Suspense>
+  );
 }
 
 export default SetPasswordPage;
