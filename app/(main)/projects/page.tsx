52a3d0ccc60ad8dacde64f1b3bcced6607620e88
Dasha <dasha2312s3@gmail.com>
2025-10-03 15:02:50 +0300

    added type for sorted

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -1,17 +1,17 @@
-import Hero from "@/components/ui/Hero";
-import {heroData} from "@/data/projects/hero";
-import { ArticleStatusEnum, ArticleTypeEnum } from "@/utils/ArticleType";
-import { ApiEndpoint } from "@/utils/http/enums/api-endpoint";
-import HttpMethod from "@/utils/http/enums/http-method";
-import ProjectPage from "@/components/projectPage/ProjectPage";
-import { IArticleBody } from "@/utils/article-content/type/interfaces";
+import Hero from '@/components/ui/Hero';
+import { heroData } from '@/data/projects/hero';
+import { ArticleStatusEnum, ArticleTypeEnum } from '@/utils/ArticleType';
+import { ApiEndpoint } from '@/utils/http/enums/api-endpoint';
+import HttpMethod from '@/utils/http/enums/http-method';
+import ProjectPage from '@/components/projectPage/ProjectPage';
+import { IArticleBody } from '@/utils/article-content/type/interfaces';
 
 async function ProjectsPage() {
-  const baseUrl = `https://api.stage.newwave4.org/api/v1/${ApiEndpoint.GET_ARTICLE_CONTENT_ALL}`;;
+  const baseUrl = `https://api.stage.newwave4.org/api/v1/${ApiEndpoint.GET_ARTICLE_CONTENT_ALL}`;
   const params = {
     currentPage: '1',
     articleType: ArticleTypeEnum.PROJECT.toString(),
-    articleStatus: ArticleStatusEnum.PUBLISHED
+    articleStatus: ArticleStatusEnum.PUBLISHED,
   };
 
   const url = new URL(baseUrl);
@@ -20,44 +20,42 @@ async function ProjectsPage() {
   try {
     const response = await fetch(url, {
       method: HttpMethod.GET,
-      next: { revalidate: 3600 },
+      //Cache
+      // next: { revalidate: 3600 },
     });
 
     if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    };
+      throw new Error('Failed to fetch data');
+    }
 
     const data = await response.json();
 
     const projects = data.content;
-    console.log('projects', projects)
+    console.log('projects', projects);
 
     return (
       <div className="ProjectsPage">
         <Hero data={heroData} />
         {projects.length > 0 ? (
-            projects.map((project: IArticleBody, index: number) => (
-              <ProjectPage key={index} project={project} />
-            ))
-          ): (
-            <div className="container mx-auto px-4 py-16">
-              <div className="text-h3 text-font-primary font-ebGaramond text-center mb-5 max-w-[600px] mx-auto">
-                Зараз сторінка &quot;Наші проєкти&quot; знаходиться на етапі розробки, але зовсім скоро ми поділимося з вами результатами!
-              </div>
-              <div className="text-center italic">Дякуємо за терпіння!</div>
+          projects.map((project: IArticleBody, index: number) => <ProjectPage key={index} project={project} />)
+        ) : (
+          <div className="container mx-auto px-4 py-16">
+            <div className="text-h3 text-font-primary font-ebGaramond text-center mb-5 max-w-[600px] mx-auto">
+              Зараз сторінка &quot;Наші проєкти&quot; знаходиться на етапі розробки, але зовсім скоро ми поділимося з вами результатами!
             </div>
-          )
-        }
+            <div className="text-center italic">Дякуємо за терпіння!</div>
+          </div>
+        )}
       </div>
     );
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error('Error fetching data:', error);
     return (
       <div className="ProjectsPage container mx-auto px-4 py-16">
         <h1>Error loading data</h1>
       </div>
     );
-  };
-};
+  }
+}
 
 export default ProjectsPage;
