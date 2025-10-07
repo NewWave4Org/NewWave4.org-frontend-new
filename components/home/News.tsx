ca3ac3ccf27202cc028a38909351d4972bf42890
Khrystyna <khrystyna.makar@gmail.com>
2025-10-07 17:51:56 +0300

    fixed filter news

--- components/home/News.tsx
+++ components/home/News.tsx
@@ -68,28 +68,6 @@ const prepareArticle = (article: Article): PreparedArticle => {
   };
 };
 
-// const news = [
-//   {
-//     id: 1,
-//     src: `${prefix}/news/news1.jpg`,
-//     title: 'Зустріч українців Брукліна з сенатором Шумером',
-//     text: '10 жовтня, відбулася зустріч представників української і проукраїнської громади цієї околиці Брукліна, НЙ з  найбільш впливовим сенатором-демократом Чаком Шумером. Його реакція на дії Путіна в Україні і, на терористичні атаки московських окупантів, здійснені у ці дні, була такою ж сповненою гніву, як кожного з нас українців. Підготовлені нами питання про визнання Росії – державою-терористом на офіційному рівні, про кваліфікацію воєнних злочинів Росії в Україні геноцидом, про надання ще більше зброї Україні знаходили глибоке розуміння і запевнення в тому, що  він, як справжній друг України, прикладе до цього всіх зусиль.v',
-//   },
-//   {
-//     id: 2,
-//     src: `${prefix}/news/news2.jpg`,
-//     title:
-//       'Український фестиваль втретє відбувся у Брукліні, в районі Брайтона',
-//     text: 'Першого червня відбувся наш фестиваль "Ангели тримають небо", зорганізований ВГО "Нова українська хвиля" та заснованою нашою організацією школою українознавства "Нова хвилька". ',
-//   },
-//   {
-//     id: 3,
-//     src: `${prefix}/news/news3.jpg`,
-//     title: 'Відзначення Дня Злуки в Українському Інституті Америки',
-//     text: 'Відзначення Дня Злуки в Українському Інституті Америки, в Нью Йорку, стало також доброю нагодою висловити вдячність Постійному Представникові України до ООН Сергію Кислиці за його вагомий внесок у боротьбі за Українські інтереси на дипломатичному фронті та за чудову співпрацю з громадою.',
-//   },
-// ];
-
 const News: React.FC<NewsProps> = ({ title = false, link, textLink }) => {
   const router = useRouter();
   const [articles, setArticles] = useState<PreparedArticle[]>([]);
@@ -103,7 +81,6 @@ const News: React.FC<NewsProps> = ({ title = false, link, textLink }) => {
     try {
       const baseUrl = `https://api.stage.newwave4.org/api/v1/${ApiEndpoint.GET_ARTICLE_CONTENT_ALL}`;
       const params = {
-        // currentPage: page.toString(),
         size: '3',
         articleType: ArticleTypeEnum.NEWS,
         articleStatus: ArticleStatusEnum.PUBLISHED,
@@ -114,9 +91,8 @@ const News: React.FC<NewsProps> = ({ title = false, link, textLink }) => {
 
       const response = await fetch(url, {
         method: HttpMethod.GET,
-        next: { revalidate: 3600 },
       });
-
+      console.log(response);
       if (!response.ok) {
         throw new Error(`Помилка завантаження новин: ${response.status}`);
       }
@@ -168,6 +144,12 @@ const News: React.FC<NewsProps> = ({ title = false, link, textLink }) => {
             ) : error ? (
               <div className="w-full text-center py-8 text-red-500">
                 {error}
+              </div>
+            ) : articles.length === 0 ? (
+              <div className="flex gap-x-6 justify-center w-full">
+                <div className="text-grey-700 text-quote">
+                  Наразі новин немає.
+                </div>
               </div>
             ) : (
               articles.map(article => (
