43a7ba9d8e15424459e8239b075cee9a6aba5f22
dhanur350 <dhanur297@gmail.com>
2025-10-14 16:05:04 +0530

    added static ENV and logs for debug

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  transpilePackages: ["nexlog"],
   images: { unoptimized: true },
   trailingSlash: false,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
