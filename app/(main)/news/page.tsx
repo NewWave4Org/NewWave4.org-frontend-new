7a05e469683573b9972487cec616631e662f0cd3
Dasha <dasha2312s3@gmail.com>
2025-10-19 15:19:51 +0300

    added small fixes

--- app/(main)/news/page.tsx
+++ app/(main)/news/page.tsx
@@ -5,17 +5,18 @@ import { heroData } from '@/data/news/hero';
 import FilterNews from '@/components/news/FilterNews';
 import NewsContent from '@/components/news/NewsContent';
 import { useState } from 'react';
+import { useSearchParams } from 'next/navigation';
 
 const NewsPage = () => {
-  const [activeFilter, setActiveFilter] = useState<number>(0);
+  const searchParams = useSearchParams();
+  const projectIdParam = searchParams.get('projectId');
+
+  const [activeFilter, setActiveFilter] = useState<number>(projectIdParam ? +projectIdParam : 0);
   return (
     <div>
       <Hero data={heroData} />
       <div className="pt-[46px] pb-[80px] relative">
-        <FilterNews
-          activeFilter={activeFilter}
-          setActiveFilter={setActiveFilter}
-        />
+        <FilterNews activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
         <NewsContent activeFilter={activeFilter} />
       </div>
     </div>
--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -1,45 +1,7 @@
-'use client';
-
-import Hero from '@/components/ui/Hero';
-import { heroData } from '@/data/projects/hero';
-import { ArticleStatusEnum, ArticleTypeEnum } from '@/utils/ArticleType';
-import ProjectPage from '@/components/projectPage/ProjectPage';
-import { IArticleBody } from '@/utils/article-content/type/interfaces';
-import { useAppDispatch, useAppSelector } from '@/store/hook';
-import { useEffect } from 'react';
-import { getAllArticle } from '@/store/article-content/action';
+import ProjectsPageClient from '@/components/projectPage/ProjectsPageClient';
 
 function ProjectsPage() {
-  const dispatch = useAppDispatch();
-  const projects = useAppSelector(state => state.articleContent.articleContent);
-
-  useEffect(() => {
-    dispatch(
-      getAllArticle({
-        page: 0,
-        articleType: ArticleTypeEnum.PROJECT,
-        articleStatus: ArticleStatusEnum.PUBLISHED,
-      }),
-    );
-  }, [dispatch]);
-
-  return (
-    <div className="ProjectsPage">
-      <Hero data={heroData} />
-      {projects.length > 0 ? (
-        projects.map((project: IArticleBody, index: number) => {
-          return <ProjectPage key={index} project={project} relevantProjectId={project.id} />;
-        })
-      ) : (
-        <div className="container mx-auto px-4 py-16">
-          <div className="text-h3 text-font-primary font-ebGaramond text-center mb-5 max-w-[600px] mx-auto">
-            Зараз сторінка &quot;Наші проєкти&quot; знаходиться на етапі розробки, але зовсім скоро ми поділимося з вами результатами!
-          </div>
-          <div className="text-center italic">Дякуємо за терпіння!</div>
-        </div>
-      )}
-    </div>
-  );
+  return <ProjectsPageClient />;
 }
 
 export default ProjectsPage;
