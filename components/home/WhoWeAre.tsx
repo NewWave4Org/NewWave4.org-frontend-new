8926b8fc6350e1cf7877e5072ee4dc0ab1b2213a
Dasha <dasha2312s3@gmail.com>
2025-12-08 17:25:20 +0200

    changed mobile styles on home page

--- components/home/WhoWeAre.tsx
+++ components/home/WhoWeAre.tsx
@@ -13,10 +13,10 @@ interface IWhoWeAre {
 const WhoWeAre: React.FC<IWhoWeAre> = ({ homeTitle, homeDescription, className }) => {
   const homeDescriptionText = convertDraftToHTML(homeDescription?.editorState);
   return (
-    <div className={`whoWeAre ${className} lg:py-0 py-4`}>
+    <div className={`whoWeAre ${className} lg:pt-0 pt-5`}>
       <div className="max-w-[1440px] lg:px-0 px-4 mx-auto lg:h-full overflow-hidden">
         <div className="flex lg:flex-row flex-col items-center lg:h-full">
-          <div className="lg:max-w-[820px] lg:ml-[96px] lg:mr-[85px] py-5">
+          <div className="lg:max-w-[820px] lg:ml-[96px] lg:mr-[85px] lg:py-5 lg:mb-0 mb-5">
             <h1 className="lg:text-h1 text-h3 text-font-primary font-ebGaramond mb-3">{homeTitle?.title}</h1>
             <div className="text-body text-font-primary" dangerouslySetInnerHTML={{ __html: homeDescriptionText }} />
           </div>
